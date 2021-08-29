41787c7b3aec43567942b0f48a5054aee53403b5
triencodonfs <89003971+triencodonfs@users.noreply.github.com>
2021-08-29 08:38:53 +0700

    fix file

--- algorithm/array/sort/heapsort/heapsort.cpp
+++ algorithm/array/sort/heapsort/heapsort.cpp
@@ -13,36 +13,7 @@ void xuatmang(int a[],int n){
 		cout<<" ";	
 	}
 }
-/*void heap(int a[],int n,int i){
-    int max=i;//gia tri lon nhat la o goc
-	int l=2*i+1;
-	int r=2*i+2;
-	
-	if(l<n && a[l]>a[max]){
-	    max=l;
-	}
-	
-	if(r<n && a[r]>a[max]){
-	    max=r;
-	} 
-	
-	if(max!=i){//neu max khong o vi tri i thi doi cho 
-	    swap(a[i],a[max]);
-	    heap(a,n,max); 
-	}
-	     
-}
 
-int sort(int a[],int n){
-    for(int i=n/2-1;i>=0;i--){//khoi tao nut 
-	   heap(a,n,i);
-	}
-	
-	for(int i=n-1;i>0;i--){
-	   swap(a[0],a[i]);//dua gia tri max ve cuoi day
-	   heap(a,n,0);
-	}
-}*/
 void heapify(int a[], int n, int i)
 {
     int max = i; 
