837c13cbbe5565df76d8cc1857038cd84dbe571b
Triển Nguyễn <89003971+triencodonfs@users.noreply.github.com>
2021-08-31 22:19:16 +0700

    Update mergesort.cpp

--- algorithm/array/sort/mergesort/mergesort.cpp
+++ algorithm/array/sort/mergesort/mergesort.cpp
@@ -16,37 +16,37 @@ void xuatmang(int a[],int n){
 		}
 }
 
-void merge(int arr[], int l, int m, int r)
+void merge(int a[], int left, int m, int r)
 {
     int i, j, k;
-    int n1 = m - l + 1;
-    int n2 =  r - m;
+    int n1 = m - left + 1;
+    int n2 = right - m;
  
   
     int L[n1], R[n2];
  
    
     for (i = 0; i < n1; i++)
-        L[i] = arr[l + i];
+        L[i] = a[left + i];
 	
     for (j = 0; j < n2; j++)
-        R[j] = arr[m + 1+ j];
+        R[j] = a[m + 1+ j];
  
    
     i = 0; 
     j = 0; // Khoi tao chi so bat dau cua mang con thu hai
-    k = l;
+    k = left;
 	
     while (i < n1 && j < n2)
     {
         if (L[i] <= R[j])
         {
-            arr[k] = L[i];
+            a[k] = L[i];
             i++;
         }
         else
         {
-            arr[k] = R[j];
+            a[k] = R[j];
             j++;
         }
         k++;
@@ -55,7 +55,7 @@ void merge(int arr[], int l, int m, int r)
     /* Copy các phan tu còn lai cua mang L vào mang a neu có */
     while (i < n1)
     {
-        arr[k] = L[i];
+        a[k] = L[i];
         i++;
         k++;
     }
@@ -63,25 +63,25 @@ void merge(int arr[], int l, int m, int r)
      /* Copy các phan tu còn lai cua mang L vào mang a neu có */
     while (j < n2)
     {
-        arr[k] = R[j];
+        a[k] = R[j];
         j++;
         k++;
     }
 }
  
 /* l là chi so trái và r là chi so phai cua mang con duoc sap xep */
-void mergeSort(int a[], int l, int r)
+void mergeSort(int a[], int left, int right)
 {
-    if (l < r)
+    if (left< right)
     {
         // Tuong tu (l+r)/2
-        int m = l+(r-l)/2;
+        int m = left +(right - left)/2;
  
         // Goi hàm de quy tiep tuc chia dôi tung nua mang
-        mergeSort(a , l, m);
-        mergeSort(a , m+1, r);
+        mergeSort(a , left, m);
+        mergeSort(a , m+1, right);
  
-        merge(a , l, m, r);
+        merge(a , left, m, right);
     }
 }
 int main(){
