bc337fa0c9d3a84d0d88689fa3a63018c88afac6
Triển Nguyễn <89003971+triencodonfs@users.noreply.github.com>
2021-08-31 22:45:25 +0700

    Update bubblesort.cpp

--- algorithm/array/sort/bubblesort/bubblesort.cpp
+++ algorithm/array/sort/bubblesort/bubblesort.cpp
@@ -5,14 +5,14 @@ using namespace std;
 void input(int a[], int n){
      for(int i = 0;i < n;i++){
 	cout<<"nhap gia tri phan tu a["<<i<<"]:";
-        cin>> a[i];	
+        cin>>a[i];	
         }
 }
 
 void output(int a[],int n){
      for(int j = 0;j < n;j++){
-	cout<< a[j];
-	cout<< " ";
+	cout<<a[j];
+	cout<<" ";
       }
 }
 
@@ -22,9 +22,9 @@ int  bubblesort(int a[],int n){
 	 for(int j = 0;j < n - 1 - i;j++){
 		 
 	     if (a[j] > a[j + 1]){
-		int tam=a[j + 1];
-		a[j + 1]=a[j];
-		a[j]= tam;
+		int tam = a[j + 1];
+		a[j + 1] = a[j];
+		a[j] = tam;
 	      }
          }
     }
