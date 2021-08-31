c77ca6fe0c798522b3bb0d9e4344c8a4466e43d5
Triển Nguyễn <89003971+triencodonfs@users.noreply.github.com>
2021-08-31 21:33:42 +0700

    Update bubblesort.cpp

--- algorithm/array/sort/bubblesort/bubblesort.cpp
+++ algorithm/array/sort/bubblesort/bubblesort.cpp
@@ -3,37 +3,39 @@
 using namespace std;
 
 void input(int a[], int n){
-	for(int i=0;i<n;i++){
-		cout<<"nhap gia tri phan tu a["<<i<<"]:";
-        cin>>a[i];	
+     for(int i = 0;i < n;i++){
+	cout<<"nhap gia tri phan tu a["<<i<<"]:";
+        cin>> a[i];	
         }
 }
 
 void output(int a[],int n){
-	for(int j=0;j<n;j++){
-		cout<<a[j];
-		cout<<" ";
-		}
+     for(int j = 0;j < n;j++){
+	cout<< a[j];
+	cout<< " ";
+      }
 }
 
 int bubblesort(int a[],int n){
-    for(int i=0;i<n;i++){
-	 for(int j=0;j<n-1-i;j++){
-	     if(a[j]>a[j+1]){
-		    int tam=a[j+1];
-		    a[j+1]=a[j];
-		    a[j]= tam;
+     for(int i = 0;i < n;i++){
+	    
+	 for(int j = 0;j < n - 1 - i;j++){
+		 
+	     if (a[j] > a[j + 1]){
+		int tam=a[j + 1];
+		a[j + 1]=a[j];
+		a[j]= tam;
 	      }
          }
     }
 }
 
 int main(){
-	int n;
-	cout<<"nhap n:";
-	cin>>n;
-	int a[n];
-	input(a,n);
-	bubblesort(a,n);
-        output(a,n);
+    int n;
+    cout<<"nhap so phan tu vao mang :";
+    cin>>n;
+    int a[n];
+    input(a,n);
+    bubblesort(a,n);
+    output(a,n);
 }	
