274d00b8c92ebe91563012cb11cd0a0315d7577c
Triển Nguyễn <89003971+triencodonfs@users.noreply.github.com>
2021-08-31 21:46:12 +0700

    Update insertsortt.cpp

--- algorithm/array/sort/insertsortt/insertsortt.cpp
+++ algorithm/array/sort/insertsortt/insertsortt.cpp
@@ -1,42 +1,45 @@
 #include<iostream>
 #include<conio.h>
 using namespace std;
+
 void input(int a[], int n){
-	for(int i=0;i<n;i++){
-           cout<<"nhap gia tri phan tu a["<<i<<"]:";
-           cin>>a[i];	
+     for (int i = 0;i < n;i++){
+         cout<<"nhap gia tri phan tu a["<<i<<"]:";
+         cin>>a[i];	
         }
 }
 
 void output(int a[],int n){
-	int j;
-	for(j=0;j<n;j++){
-	   cout<<a[j];
-	   cout<<" ";
-	}
+     int j;
+     for(j = 0;j < n;j++){
+        cout<<a[j];
+	cout<<" ";
+     }
 }
 
-int insertsort(int a[], int n){
-	int i;
-        for(i=0;i<n;i++){
-	   int j=i-1;
-	   int index=a[i];
-	   int tam;
+int  insertsort(int a[], int n)
+{
+     int i;
+     for(i = 0;i < n;i++){
+	 int j=i - 1;
+	 int index = a[i];
+	 int tam;
 		
-          while(a[j]>index&&j>=0){
-                a[j+1]=a[j];
-                j--;
+         while(a[j] > index&&j >= 0){
+               a[j + 1]=a[j];
+               j--;
 	}
-	a[j+1]=index;
+	a[j + 1] = index;
 	}
  }
 
-int main(){
-	int n;
-	cout<<"nhap n:";
-	cin>>n;
-	int a[n];
-	input(a,n);
-	insertsort(a,n);
-        output(a,n);
+int  main()
+{
+     int n;
+     cout<<"nhap so phan tu vao mang :";
+     cin>>n;
+     int a[n];
+     input(a,n);
+     insertsort(a,n);
+     output(a,n);
 }
