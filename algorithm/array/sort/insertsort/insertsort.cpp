dbce152e425df9f4b87e83f802942fe4eb8640ad
Triển Nguyễn <89003971+triencodonfs@users.noreply.github.com>
2021-09-02 08:17:43 +0700

    Update insertsort.cpp

--- algorithm/array/sort/insertsort/insertsort.cpp
+++ algorithm/array/sort/insertsort/insertsort.cpp
@@ -33,7 +33,8 @@ int  insertsort(int a[], int n)
          while(a[j] > index && j >= 0)
 	 {
                a[j + 1] = a[j];
-               j--;
+               j--;                                                   
+	       i--;                       	  
          }
 	 a[j + 1] = index;
      }
